c47be19c0dfa05a47412238a949193802ff4456a
kiki <ki367159@gmail.com>
2019-01-23 16:53:38 +0800

    0123

---
+++ users/views.py
@@ -0,0 +1,51 @@
+from rest_framework import viewsets
+from rest_framework.response import Response
+from rest_framework.decorators import action
+from rest_framework.permissions import (
+    IsAdminUser,
+    IsAuthenticatedOrReadOnly,
+)
+from .models import User, Relationship
+from .serializers import UserSerializer, RelationshipSerializers
+from .permissions import IsSelf
+
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly]
+
+    def get_serializer_class(self):
+        serializer = super().get_serializer_class()
+        if self.action == 'follow':
+            return RelationshipSerializers
+
+        return serializer
+
+    def get_permissions(self):
+        permissions = super().get_permissions()
+        if self.action == 'create':
+            return []
+
+        if self.action == 'destory':
+            permissions.append(IsAdminUser())
+
+        if self.action in ['update', 'partial_update']:
+            permissions.append(IsSelf())
+
+        return permissions
+
+    @action(['POST'], True)
+    def follow(self, request, pk):
+        to_user = self.get_object()
+        from_user = request.user
+
+        serializer = self.get_serializer(data={
+            'to_user': to_user.id,
+            'from_user': from_user.id,
+            'is_agree': to_user.is_public,
+        })
+
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response({'status': 'ok'})
